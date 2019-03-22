8b2947fa0a5601c0c1adcbbaa71735df350e14ae
chief <chief_fei@163.com>
2019-03-22 23:02:14 +0800

    merge  zf

--- src/pages/Account/Center/Applications.js
+++ src/pages/Account/Center/Applications.js
@@ -24,7 +24,7 @@ class Center extends PureComponent {
           <a target="_blank" rel="noopener noreferrer" href="https://www.taobao.com/">
             2nd menu item
           </a>
-          <div>qdp branch test</div>
+          <div>zf etstndjksfkkf34234 branch test <h1>merge test</h1></div˚>
         </Menu.Item>
         <Menu.Item>
           <a target="_blank" rel="noopener noreferrer" href="https://www.tmall.com/">
