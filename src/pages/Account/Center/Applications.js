930fe06ac90a3a1792d137495da828522c2cbe29
qiandapeng <920121402@qq.com>
2019-03-22 23:10:11 +0800

    'up'

--- src/pages/Account/Center/Applications.js
+++ src/pages/Account/Center/Applications.js
@@ -24,7 +24,7 @@ class Center extends PureComponent {
           <a target="_blank" rel="noopener noreferrer" href="https://www.taobao.com/">
             2nd menu item
           </a>
-          <div>qdp branch test merge test kakkakakakak <span>jsfhlnfjsdiuf</span></div>
+          <div>qdp branch test merge test kakkakakakak <a>12324</a><span>jsfhlnfjsdiuf</span></div>
         </Menu.Item>
         <Menu.Item>
           <a target="_blank" rel="noopener noreferrer" href="https://www.tmall.com/">
